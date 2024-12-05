06d6b079088b99f8a45ed05880f653e0d36bb183
jhb <2695210554@qq.com>
2024-12-05 20:34:28 +0800

    添加项目文件。

---
+++ Context/TraceContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Trace_Api.Context
+{
+    public class TraceContext:DbContext
+    {
+
+    }
+}
---
+++ Model/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+
+namespace Trace_Api.Model
+{
+    public class BaseEntity
+    {
+
+        public int Id { get; set; }
+        public DateTime CreateDataTime { get; set; }
+        public DateTime UpdateDataTime { get; set; }
+    }
+}
